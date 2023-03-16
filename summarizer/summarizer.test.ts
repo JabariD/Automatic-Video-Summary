cc578bfcf79fa8a0653ff76175bdf1a464e41db8
Payton Dennis <dennispayton15@gmail.com>
2023-03-15 23:38:30 -0700

    [Summarizer] Fix reliance on public method

--- summarizer/summarizer.test.ts
+++ summarizer/summarizer.test.ts
@@ -3,7 +3,6 @@ import { VideoSegment } from "../transcript/youtube/youtube";
 
 describe("Summarizer", () => {
   it("should normalize text", async () => {
-    const summarizer = new Summarizer();
     const videoSegments: VideoSegment[] = [
       {
         text: "All right, so here we are in front of the elephants,\n    very cool",
@@ -11,20 +10,25 @@ describe("Summarizer", () => {
         offset: 0,
       },
     ];
-    const summary = await summarizer.getSummary(videoSegments);
-    expect(summary).toBe(
+
+    // TODO(JabariD): Refactor this to use the Summarizer class instead of the Summarizer class's private methods. (https://dev.to/danywalls/testing-private-methods-in-typescript-3np5)
+    const sanitizedText = new Summarizer()["normalizeText"](videoSegments);
+
+    expect(sanitizedText).toBe(
       "All right, so here we are in front of the elephants, very cool"
     );
   });
 
   it("should normalize multiple segments", async () => {
-    const summarizer = new Summarizer();
     const videoSegments: VideoSegment[] = [
       { text: "foo-bar    \n @", duration: 3000, offset: 0 },
       { text: "normalized", duration: 3000, offset: 3000 },
       { text: "     checker,@  \n", duration: 3000, offset: 6000 },
     ];
-    const summary = await summarizer.getSummary(videoSegments);
-    expect(summary).toBe("foo-bar normalized checker,");
+
+    // TODO(JabariD): Refactor this to use the Summarizer class instead of the Summarizer class's private methods. (https://dev.to/danywalls/testing-private-methods-in-typescript-3np5)
+    const sanitizedText = new Summarizer()["normalizeText"](videoSegments);
+
+    expect(sanitizedText).toBe("foo-bar normalized checker,");
   });
 });
